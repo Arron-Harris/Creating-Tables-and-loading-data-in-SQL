728031889d0f195e03fb622db2d3f0d3dbaa7339
Arron-Harris <58215393+Arron-Harris@users.noreply.github.com>
2022-02-05 23:44:23 -0500

    Add files via upload

---
+++ Create-Table-and-Load-Data.sql
@@ -0,0 +1,28 @@
+-- SQL for Data Analysis
+-- Chapter: Statistics with SQL
+-- Lecture: Create Table and Load Data
+
+CREATE DATABASE snp500
+GO
+
+USE snp500
+GO
+
+CREATE TABLE CompanyBySector
+   (Ticker varchar(5) 
+   ,CompanyName varchar(50) 
+   ,Sector varchar(50)
+   ,StockPrice decimal(6,2)
+   )  
+GO
+
+BULK INSERT CompanyBySector
+FROM 'C:\Users\Admin122422\Downloads\snp500-company-by-sector.csv'
+WITH
+(
+    FIRSTROW = 2,          
+    FIELDTERMINATOR = ';',  
+    ROWTERMINATOR = '\n',   
+    TABLOCK                 
+)
+GO
